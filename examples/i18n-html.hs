9bf6aa488a3e4be605ee6b6a6e69e509bb97775a
Nils Schweinsberg <mail@nils.cc>
2014-08-26 14:02:35 +0000

    Html5 example

---
+++ examples/i18n-html.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Text.I18n
+import Text.I18n.Po hiding (putStrLn)
+import Text.Blaze.I18n
+import Text.Blaze.Renderer.Pretty
+
+import qualified Blaze.Html5 as H
+
+import System.Environment
+
+main :: IO ()
+main = do
+
+  -- get locale
+  args <- getArgs
+  env  <- getEnv "LANG"
+  let locale = Locale $ head (args ++ [takeWhile ('_' /=) env, "en"])
+
+  -- load L10n from PO file
+  po <- getL10n "."
+
+  putStrLn $ case po of
+    (ln, []) -> renderMarkup $ localizeMarkup ln locale html
+    (_, err) -> "Errors loading po file: " ++ show err
+
+html :: H.Html
+html = H.docTypeHtml $ do
+
+  H.h1 $ i18n "Hello, %s!" ["Joe"]
+  H.p  $ i18n "This is a test." []
